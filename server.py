e94cd4f482b61cbbe255eaa57814d725a46f5a99
plamendimitrov <pgdimitrov@yahoo.com>
2025-09-11 16:29:34 +0300

    server fix

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 import os
 import sys
 
-# Add src to path
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'src'))
+# Add src to Python path
+src_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'src')
+sys.path.insert(0, src_path)
 
+# Import main
 from main import main
 
 if __name__ == "__main__":
