101901db172873fe6d6c9f1e4eb180d8b51df0ee
plamendimitrov <pgdimitrov@yahoo.com>
2025-10-30 14:36:09 +0200

    new ver test

--- src/version.py
+++ src/version.py
@@ -1,1 +1,7 @@
-version = {'version': '1700', 'build': '001', 'release_date': '2024-01-01'}
+version = {
+    'version': '0.1.0',
+    'major': 0,
+    'minor': 1, 
+    'patch': 0,
+    'build_date': '2024-01-01 10:00:00'
+}
