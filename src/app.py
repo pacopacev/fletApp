b97786b775b118f2765fc5229842122050f3822d
pacopacev <pgdimitrov@yahoo.com>
2025-10-30 20:27:56 +0200

    sasasadwqe

--- src/app.py
+++ src/app.py
@@ -15,7 +15,6 @@ import importlib.util
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
-
 try:
     from version import version
     print(f"App Version: {version['version']}")
