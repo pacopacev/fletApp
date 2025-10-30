25eb6e9c6daf592a2c1bc3e307aef5fde2e560f3
pacopacev <pgdimitrov@yahoo.com>
2025-10-30 19:07:17 +0200

    may fix

--- version.py
+++
@@ -1,9 +0,0 @@
-# version.py
-version = {
-    'version': '1.0.2',
-    'major': 1,
-    'minor': 0,
-    'patch': 2,
-    'build_date': '2025-10-30 14:59:52',
-    'commit_hash': 'd806224ec4837862f51f6a3f03bb2f026e93e234'
-}
