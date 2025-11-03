10fc151bdbe3be624227a9cd3b75f4c9164a9a84
plamendimitrov <pgdimitrov@yahoo.com>
2025-11-03 14:23:39 +0200

    ver fix

---
+++ version.py
@@ -0,0 +1,9 @@
+# version.py
+version = {
+    'version': '1.0.15',
+    'major': 1,
+    'minor': 0,
+    'patch': 15,
+    'build_date': '2025-10-30 19:00:14',
+    'commit_hash': '3f7ac5a6e1d80789302f1dd91592a0f4b7db1e7e'
+}
