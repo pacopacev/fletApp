7342fd6c1dfd7d7b059756157fe7c896fa06af2a
pacopacev <pgdimitrov@yahoo.com>
2025-11-11 18:48:47 +0200

    fix 19

--- version.py
+++
@@ -1,9 +0,0 @@
-# version.py
-version = {
-    'version': '1.0.21',
-    'major': 1,
-    'minor': 0,
-    'patch': 21,
-    'build_date': '2025-11-10 17:53:14',
-    'commit_hash': '021fd464528ee60073825aaad72704d7c740eda7'
-}
